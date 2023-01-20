1e14d38b91ce7a9c3feaab30814f88059df4ffe3
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-01-20 17:57:58 +0000

    Boocle finalizado

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
-    //creacion de arrys 
-    let pronoun = ['the', 'our', "you"];
-    let adj = ['good', 'little', 'awesome'];
-    let noun = ['doct', 'universe', 'market'];
-    let dom = ['.com', '.net', '.org'];
+//creacion de arrys 
+let pronoun = ['the', 'our', "you"];
+let adj = ['good', 'little', 'awesome'];
+let noun = ['doct', 'universe', 'market'];
+let dom = ['.com', '.net', '.io'];
 
-    // usar los bucles o loops anidados para mezclar los diferentes valores
-    for (let p = 0; p < pronoun.length; p++) {
-        for (let a = 0; a < adj.length; a++) {
-            for (let n = 0; n < noun.length; n++) {
-                for (let d = 0; d < dom.length; d++) {
-                    console.log(pronoun[p] + adj[a] + noun[n] + dom[d]); // Une dominios sin excepcion//
-                }
-                if (noun === 'org') {
-                    console.log(pronoun[p] + adj[a] + noun[n].slice(0, -2) + ext[2]);
-                }
+// usar los bucles o loops anidados para mezclar los diferentes valores
+for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            for (let d = 0; d < dom.length; d++) {
+                console.log(pronoun[p] + adj[a] + noun[n] + dom[d]); // Une dominios sin excepcion//
+                // domain hacks//
+
             }
+
         }
     }
+}
 
 
 
